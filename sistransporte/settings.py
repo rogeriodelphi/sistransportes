7cd839a8ff429b8074aa1d5ef610bd7d7383a75e
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-16 16:48:35 -0300

    Atualizando o settings.py

--- sistransporte/settings.py
+++ sistransporte/settings.py
@@ -30,7 +30,6 @@ sys.path.insert(0, APPS_DIR)
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
