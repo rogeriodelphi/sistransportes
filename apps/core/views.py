d4cf6e80f9d54a5f7d7aa071fcc04f6c63aeae37
rogeriodelphi <rogeriodelphi@gmail.com>
2021-01-16 16:52:43 -0300

    Configurando arquivos para Deploy no Heroku

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-# @login_required(login_url='/contas/login/')
 def index(request):
     return render(request, 'index.html')
