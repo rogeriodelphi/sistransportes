a0de806817e00d02822c35569b049535095059a4
rogeriodelphi <rogeriodelphi@gmail.com>
2021-03-13 23:31:31 -0300

    Atualizando os arquivos estáticos

---
+++ static/vendor/jquery/masks.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.date-mask').mask('00/00/0000');
+    $('.date-mask-placeholder').mask('00/00/0000', {placeholder: "__/__/____"});
+    $('.phone-mask').mask('(00) 00000-0000');
+    $('.cpf-mask').mask('000.000.000-00');
+    $('.datepicker').datepicker({
+        dateFormat: "dd/mm/yy",
+    });
+});
